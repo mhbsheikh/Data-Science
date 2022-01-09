c4fd4486d932f41f3962df6bcbf11de2e3192846
Mehedi Hasan Bijoy <58245357+mehedihasanbijoy@users.noreply.github.com>
2022-01-09 13:06:21 +0600

    Update tempTable.sql

--- MySQL/tempTable.sql
+++ MySQL/tempTable.sql
@@ -18,3 +18,33 @@ insert into tempEmployees
 select emp_id, emp_name, age, gender from employees;
 
 select * from tempEmployees;
+
+
+
+
+
+#########################################
+# CTE - Common Table Expression
+#########################################
+
+with temp_emps (id, name, salary)
+as(
+	select 
+		emp_id,
+		concat(first_name, ' ', last_name) as emp_name,
+		salary
+	from employees
+) select * from temp_emps;
+
+
+
+with all_players (id, name)
+as(
+	select * from cricket c
+	union all
+	select * from football f
+	union all
+	select * from rugby r
+) select * from all_players a, all_players b
+where a.id = b.id and a.name <> b.name;
+
